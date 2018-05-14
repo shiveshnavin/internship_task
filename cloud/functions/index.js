0e9fd8f54d3be425e87283cb74252f2658e0e43e
shiveshnavin <shiveshnavin@gmail.com>
2018-05-14 13:54:56 +0530

    Added Firebase Funtions Init

--- cloud/functions/index.js
+++ cloud/functions/index.js
@@ -1,4 +1,7 @@
 const functions = require('firebase-functions');
+const admin=require('firebase-admin')
+
+admin.initializeApp(functions.config().firebase);
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -7,10 +10,18 @@ const functions = require('firebase-functions');
 //  response.send("Hello from Firebase!");
 // });
 
-exports.viewall= functions.https.onRequest((request, response) => {
+exports.dml_viewall= functions.https.onRequest((request, response) => {
 
-
+ 
   response.send("Hello from Firebase!");
 
-  
+
+});
+
+exports.dml_update= functions.https.onRequest((request, response) => {
+
+ 
+  response.send("Hello from Firebase!");
+
+
 });
